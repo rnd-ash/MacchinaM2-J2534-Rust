86056566bfdf5f7b08275ab7b1b77c5b159003a8
Ashcon Mohseninia <ashconm@outlook.com>
2020-10-11 19:53:40 +0100

    Formatting

--- Driver/src/lib.rs
+++ Driver/src/lib.rs
@@ -1,7 +1,7 @@
-use J2534Common::*;
+use libc;
 use libc::{c_char, c_long};
 use std::ffi::CString;
-use libc;
+use J2534Common::*;
 mod logger;
 
 const API_VERSION: &str = "04.04";
@@ -22,7 +22,13 @@ pub extern "system" fn PassThruClose(pDeviceID: i32) -> i32 {
 
 #[no_mangle]
 #[allow(non_snake_case)]
-pub extern "system" fn PassThruConnect(DeviceID: i32, ProtocolID: i32, Flags: i32, BaudRate: i32, pChannelID: *mut i32) -> i32 {
+pub extern "system" fn PassThruConnect(
+    DeviceID: i32,
+    ProtocolID: i32,
+    Flags: i32,
+    BaudRate: i32,
+    pChannelID: *mut i32,
+) -> i32 {
     PassthruError::STATUS_NOERROR as i32
 }
 
@@ -34,14 +40,29 @@ pub extern "system" fn PassThruDisconnect(ChannelID: i32) -> i32 {
 
 #[no_mangle]
 #[allow(non_snake_case)]
-pub extern "system" fn PassThruReadVersion(DeviceID: i32, pFirmwareVersion: *mut c_char, pDllVersion: *mut c_char, pApiVersion: *mut c_char) -> i32 {
-    let fw = CString::new(FW_VERSION).unwrap();
-    let api = CString::new(API_VERSION).unwrap();
-    let dll = CString::new(DLL_VERSION).unwrap();
-    unsafe {
-        std::ptr::copy_nonoverlapping( fw.as_c_str().as_ptr(), pFirmwareVersion, FW_VERSION.len());
-        std::ptr::copy_nonoverlapping( api.as_c_str().as_ptr(), pApiVersion, API_VERSION.len());
-        std::ptr::copy_nonoverlapping( dll.as_c_str().as_ptr(), pDllVersion, DLL_VERSION.len());
+pub extern "system" fn PassThruReadVersion(
+    DeviceID: i32,
+    pFirmwareVersion: *mut c_char,
+    pDllVersion: *mut c_char,
+    pApiVersion: *mut c_char,
+) -> i32 {
+    match CString::new(FW_VERSION) {
+        Err(e) => return PassthruError::ERR_FAILED as i32,
+        Ok(s) => unsafe {
+            std::ptr::copy_nonoverlapping(s.as_c_str().as_ptr(), pFirmwareVersion, FW_VERSION.len())
+        },
+    }
+    match CString::new(DLL_VERSION) {
+        Err(e) => return PassthruError::ERR_FAILED as i32,
+        Ok(s) => unsafe {
+            std::ptr::copy_nonoverlapping(s.as_c_str().as_ptr(), pDllVersion, DLL_VERSION.len())
+        },
+    }
+    match CString::new(API_VERSION) {
+        Err(e) => return PassthruError::ERR_FAILED as i32,
+        Ok(s) => unsafe {
+            std::ptr::copy_nonoverlapping(s.as_c_str().as_ptr(), pApiVersion, API_VERSION.len())
+        },
     }
     PassthruError::STATUS_NOERROR as i32
 }
@@ -54,13 +75,25 @@ pub extern "system" fn PassThruGetLastError(pErrorDescription: *mut c_char) -> i
 
 #[no_mangle]
 #[allow(non_snake_case)]
-pub extern "system" fn PassThruReadMsgs(ChannelID: i32, pMsg: *mut PASSTHRU_MSG, pNumMsgs: *mut i32, Timeout: i32) -> i32 {
+pub extern "system" fn PassThruReadMsgs(
+    ChannelID: i32,
+    pMsg: *mut PASSTHRU_MSG,
+    pNumMsgs: *mut i32,
+    Timeout: i32,
+) -> i32 {
     PassthruError::STATUS_NOERROR as i32
 }
 
 #[no_mangle]
 #[allow(non_snake_case)]
-pub extern "system" fn PassThruStartMsgFilter(ChannelID: i32, FilterType: i32, pMaskMsg: *const PASSTHRU_MSG, pPatternMsg: *const PASSTHRU_MSG, pFlowControlMsg: *const PASSTHRU_MSG, pMsgID: *mut i32) -> i32 {
+pub extern "system" fn PassThruStartMsgFilter(
+    ChannelID: i32,
+    FilterType: i32,
+    pMaskMsg: *const PASSTHRU_MSG,
+    pPatternMsg: *const PASSTHRU_MSG,
+    pFlowControlMsg: *const PASSTHRU_MSG,
+    pMsgID: *mut i32,
+) -> i32 {
     PassthruError::STATUS_NOERROR as i32
 }
 
@@ -72,13 +105,23 @@ pub extern "system" fn PassThruStopMsgFilter(ChannelID: i32, MsgID: i32) -> i32 
 
 #[no_mangle]
 #[allow(non_snake_case)]
-pub extern "system" fn PassThruWriteMsgs(ChannelID: i32, pMsg: *const PASSTHRU_MSG, pNumMsgs: *const i32, Timeout: i32) -> i32 {
+pub extern "system" fn PassThruWriteMsgs(
+    ChannelID: i32,
+    pMsg: *const PASSTHRU_MSG,
+    pNumMsgs: *const i32,
+    Timeout: i32,
+) -> i32 {
     PassthruError::STATUS_NOERROR as i32
 }
 
 #[no_mangle]
 #[allow(non_snake_case)]
-pub extern "system" fn PassThruStartPeriodicMsg(ChannelID: i32, pMsg: *const PASSTHRU_MSG, pMsgID: *const i32, TimeInterval: i32) -> i32 {
+pub extern "system" fn PassThruStartPeriodicMsg(
+    ChannelID: i32,
+    pMsg: *const PASSTHRU_MSG,
+    pMsgID: *const i32,
+    TimeInterval: i32,
+) -> i32 {
     PassthruError::STATUS_NOERROR as i32
 }
 
@@ -90,41 +133,59 @@ pub extern "system" fn PassThruStopPeriodicMsg(ChannelID: i32, MsgID: i32) -> i3
 
 #[no_mangle]
 #[allow(non_snake_case)]
-pub extern "system" fn PassThruIoctl(HandleID: i32, IoctlID: i32, pInput: *mut libc::c_void, pOutput: *mut libc::c_void) -> i32 {
+pub extern "system" fn PassThruIoctl(
+    HandleID: i32,
+    IoctlID: i32,
+    pInput: *mut libc::c_void,
+    pOutput: *mut libc::c_void,
+) -> i32 {
     PassthruError::STATUS_NOERROR as i32
 }
 
 #[no_mangle]
 #[allow(non_snake_case)]
-pub extern "system" fn PassThruSetProgrammingVoltage(DeviceID: i32, PinNumber: i32, Voltage: i32) -> i32 {
+pub extern "system" fn PassThruSetProgrammingVoltage(
+    DeviceID: i32,
+    PinNumber: i32,
+    Voltage: i32,
+) -> i32 {
     PassthruError::STATUS_NOERROR as i32
 }
-
 
 #[test]
 fn test_read_version() {
     let mut api_version: [u8; 80] = [0; 80];
     let mut dll_version: [u8; 80] = [0; 80];
-    let mut fw_version:  [u8; 80] = [0; 80];
+    let mut fw_version: [u8; 80] = [0; 80];
     let res = PassThruReadVersion(
-        0, 
-        fw_version.as_mut_ptr() as *mut libc::c_char, 
+        0,
+        fw_version.as_mut_ptr() as *mut libc::c_char,
         dll_version.as_mut_ptr() as *mut libc::c_char,
-        api_version.as_mut_ptr() as *mut libc::c_char
+        api_version.as_mut_ptr() as *mut libc::c_char,
     );
 
     assert!(res == PassthruError::STATUS_NOERROR as i32);
 
     unsafe {
-        let api = String::from(std::ffi::CStr::from_ptr(api_version.as_mut_ptr() as *mut libc::c_char).to_str().unwrap());
+        let api = String::from(
+            std::ffi::CStr::from_ptr(api_version.as_mut_ptr() as *mut libc::c_char)
+                .to_str()
+                .unwrap(),
+        );
         assert!(api.as_str() == API_VERSION);
 
-        let dll = String::from(std::ffi::CStr::from_ptr(dll_version.as_mut_ptr() as *mut libc::c_char).to_str().unwrap());
+        let dll = String::from(
+            std::ffi::CStr::from_ptr(dll_version.as_mut_ptr() as *mut libc::c_char)
+                .to_str()
+                .unwrap(),
+        );
         assert!(dll.as_str() == DLL_VERSION);
 
-        let fw = String::from(std::ffi::CStr::from_ptr(fw_version.as_mut_ptr() as *mut libc::c_char).to_str().unwrap());
+        let fw = String::from(
+            std::ffi::CStr::from_ptr(fw_version.as_mut_ptr() as *mut libc::c_char)
+                .to_str()
+                .unwrap(),
+        );
         assert!(fw.as_str() == FW_VERSION);
     }
-
-
 }
--- Driver/src/logger.rs
+++ Driver/src/logger.rs
@@ -1,48 +1,16 @@
-use std::io;
+use std::fmt::Formatter;
 use std::fs::File;
+use std::io;
 use std::io::prelude::*;
 use std::sync::{Arc, Mutex};
-use std::fmt::Formatter;
+use winapi::*;
 
 #[cfg(target_family = "windows")]
-const LOG_PATH : &str = "C:\\Program Files (x86)\\macchina\\passthru\\driver_log.txt";
+const LOG_PATH: &str = "C:\\Program Files (x86)\\macchina\\passthru\\driver_log.txt";
 #[cfg(target_family = "unix")]
-const LOG_PATH : &str = "/usr/share/macchina/passthru/driver_log.txt";
+const LOG_PATH: &str = "/usr/share/macchina/passthru/driver_log.txt";
 
-pub enum LogLevel {
-    Debug,
-    Info,
-    Warn,
-    Error,
-}
+struct Logger {}
 
-impl std::fmt::Display for LogLevel {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match *self {
-            LogLevel::Debug => write!(f, "DEBUG"),
-            LogLevel::Info => write!(f, "INFO"),
-            LogLevel::Warn => write!(f, "WARN"),
-            LogLevel::Error => write!(f, "ERROR")
-        }
-    }
-}
-
-
-
-
-pub struct Logger {
-    logFile: File
-}
-
-impl Logger {
-    pub fn init() -> std::io::Result<Logger> {
-        let file = File::create(LOG_PATH)?;
-        Ok(Logger { logFile: file })
-    }
-
-    pub fn log_msg(&mut self, level: LogLevel, msg: String) {
-        if self.logFile.write_all(format!("[{}]: {}", level, msg).as_bytes()).is_err() {
-            println!("Error writing to logfile!. Msg is {}-{}", level, msg)
-        }
-    }
-}
+#[cfg(target_family = "windows")]
+impl Logger {}
