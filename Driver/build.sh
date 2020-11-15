f77f812281375f026d916f0483b63b1fe76a4ef1
Ashcon Mohseninia <ashconm@outlook.com>
2020-11-15 14:47:32 +0000

    Move UNIX Driver path to ~/.passthru

--- Driver/build.sh
+++ Driver/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cargo build --release
-sudo cp target/release/libm2_driver.so /usr/share/passthru/macchina.so
+sudo cp ../macchina.json ~/.passthru/macchina.so
+sudo cp target/release/libm2_driver.so ~/.passthru/macchina.so
