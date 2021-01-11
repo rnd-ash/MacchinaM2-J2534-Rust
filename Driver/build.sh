abd6657c5582bbb91cb4dfcfc7ea564caea92dac
Daniel Cuthbert <daniel@seventysix.io>
2021-01-11 15:01:50 +0000

    Updated build script for MacOS

--- Driver/build.sh
+++ Driver/build.sh
@@ -10,9 +10,10 @@ else
 fi
 
 echo "Copying JSON to ~/.passthru/"
-cp macchina.json ~/.passthru/macchina.json
+cp ../macchina.json ~/.passthru/macchina.so
 
+#This is for macOS, uncomment out for Linux"
 echo "Copying library to ~/.passthru/"
-cp target/${path}/libm2_driver.so ~/.passthru/macchina.so
-
-echo "Complete"
+#cp target/${path}/libm2_driver.so ~/.passthru/macchina.so
+cp target/${path}/libm2_driver.dylib ~/.passthru/macchina.dylib
+echo "Driver install is complete. Happy car hacking!"
